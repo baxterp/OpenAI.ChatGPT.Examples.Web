9372facc101ba7fcf6ea227dbeb94c9d146dc981
Baxter Pearson <baxterrpearson@gmail.com>
2025-11-28 15:49:53 +0000

    Checkin

---
+++ OpenAI.ChatGPT.Examples.Web/Controllers/DocumentUploadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenAI.ChatGPT.Examples.Web.Controllers
+{
+    public class DocumentUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
